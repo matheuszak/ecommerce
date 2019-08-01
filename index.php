2674a0c16a2315272af06138fd878cbe5af149a7
matheus <matheuszak@hotmail.com>
2019-08-01 18:13:20 -0300

    autoload hcode classes

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$sql = new Hcode\DB\SqL();
+
+	$results = $sql->select("SELECT * FROM tb-users");
+
+	echo json_encode($results);
 
 });
 
